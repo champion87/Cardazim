0303ede214af5c15bab115ed0c6e18f77d343bcf
Lidor Matityahou <lidor071205@gmail.com>
2024-11-30 14:09:43 +0200

    extended 'pack_message' for byte objects

--- cardazim/utils.py
+++ cardazim/utils.py
@@ -1,4 +1,5 @@
 import struct
+from typing import Union
 
 """The protocol:
 'MSIZE_NBYTES' bytes that describe the length of the message,
@@ -7,8 +8,16 @@ followed by the bytes of the message."""
 MSIZE_NBYTES = 4
 MSIZE_FMT = '<I' # I is for a 4-byte unsigned integer, < i for little endian
 
-def pack_message(message: str) -> bytes:
-    message_bytes = message.encode('utf-8')
+def pack_message(message: Union[str, bytes]) -> bytes:
+    """
+    Packs a message by encoding it to bytes (if necessary) and prepending the message length.
+    
+    :param message: The message to pack, either as a string or bytes.
+    :type message: Union[str, bytes]
+    :returns: The packed message as bytes.
+    :rtype: bytes
+    """
+    message_bytes = message.encode('utf-8') if isinstance(message, str) else message
     msg_len = len(message_bytes)
     return struct.pack(MSIZE_FMT, msg_len) + message_bytes
 
