2d30f43341d6628994b9cc4921bacbb126912eb3
Lidor Matityahou <lidor071205@gmail.com>
2024-11-30 18:23:18 +0200

    finished implementing 'Connection'

--- cardazim/connection.py
+++ cardazim/connection.py
@@ -1,33 +1,64 @@
 from __future__ import annotations
 import socket
-from utils import pack_message
+from utils import pack_message, unpack_message, init_server_socket
+
+RECV_BUFSIZE = 4096
 
 class Connection:
     
     def __init__(self, connection: socket.socket):
         self.connection: socket.socket = connection
 
+
     @classmethod
-    def connect(cls, target_host_ip:str, port:int) -> Connection:
-        pass
+    def connect(cls, host_ip:str, port:int) -> Connection:
+        connection : socket.socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        connection.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+        connection.connect((host_ip, port))
+        return cls(connection)
+
 
     def close(self) -> None:
         self.connection.close()
 
+
     def __repr__(self) -> str:
-        return f"<{self.__class__.__name__} from {self.connection.getsockname()} to {self.connection.getsockname()}>"
+        return f"<{self.__class__.__name__} from {self.connection.getsockname()} to {self.connection.getpeername()}>"
+
 
     def send_message(self, message: bytes):
-        client.sendall(pack_message(message))
+        """
+        Sends a message to the connected peer.
+        
+        :param message: The message to send.
+        :type bytes:
+        :raises Exception: If the sending fails.
+        """
+        self.connection.sendall(pack_message(message))
 
-    def recieve_message(self):
+
+    def recieve_message(self) -> bytes:
         """
-        :raises Exception: If the connection was closed before the message was fully recieved
+        Receives a message from the connected peer.
+        
+        :raises Exception: If the connection is closed before the message is fully received.
+        :return: the recieved msg.
+        :rtype: bytes
         """
-        pass
+        from_client = b""
+
+        while True:
+            if not (data := self.connection.recv(RECV_BUFSIZE)):
+                break
+
+            from_client += data
+
+        return unpack_message(from_client, decode_result=False)
+
 
     def __enter__(self):
         return self
+
 
     def __exit__(self, exc_type, exc_val, exc_tb):
         self.close()
--- cardazim/utils.py
+++ cardazim/utils.py
@@ -1,5 +1,6 @@
 import struct
 from typing import Union
+import socket
 
 """The protocol:
 'MSIZE_NBYTES' bytes that describe the length of the message,
@@ -7,6 +8,8 @@ followed by the bytes of the message."""
 
 MSIZE_NBYTES = 4
 MSIZE_FMT = '<I' # I is for a 4-byte unsigned integer, < i for little endian
+BACKLOG_SIZE = 1
+
 
 def pack_message(message: Union[str, bytes]) -> bytes:
     """
@@ -21,7 +24,20 @@ def pack_message(message: Union[str, bytes]) -> bytes:
     msg_len = len(message_bytes)
     return struct.pack(MSIZE_FMT, msg_len) + message_bytes
 
-def unpack_message(packed_data: bytes) -> str:
+
+def unpack_message(packed_data: bytes, decode_result: bool = True) -> Union[str, bytes]:
+    """
+    Unpacks a message from its the data according to the protocol.
+
+    :param packed_data: The message data.
+    :type packed_data: bytes
+    :param decode_result: If True, the message will be decoded to a string using UTF-8; 
+                          otherwise, it will be returned as raw bytes.
+    :type decode_result: bool
+    :return: The unpacked message, either as a string (if `decode_result` is True) or as bytes.
+    :rtype: Union[str, bytes]
+    :raises Exception: If the data isn't valid according to the protocol.
+    """
     if len(packed_data) < MSIZE_NBYTES:
         raise Exception("The recieved data is too short to include the bytes for the message size")
 
@@ -31,4 +47,21 @@ def unpack_message(packed_data: bytes) -> str:
     if len(msg_bytes) != msg_len:
         raise Exception("The recieved message's length differ from the declared length")
 
-    return packed_data[MSIZE_NBYTES:MSIZE_NBYTES + msg_len].decode('utf-8')
+    return msg_bytes.decode('utf-8') if decode_result else msg_bytes
+
+
+def init_server_socket(server_ip: str, server_port: int) -> socket.socket:
+    """
+    Creates and initializes a socket for the server.
+
+    :param server_ip: 
+    :type str:
+    :param server_port:
+    :type int:
+    """
+    server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    server_socket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+    server_socket.bind((server_ip, server_port))
+    server_socket.listen(BACKLOG_SIZE) # This is how many queued connections do we support.
+
+    return server_socket
